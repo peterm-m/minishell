1df70fe7651a4f9474cf051b41f7015aacc1e60f
Alejandro Diaz-Ufano Perez <adiazufano13@gmail.com>
2024-02-08 11:24:34 +0100

    pull

--- src/builtins/cd_builtin.c
+++ src/builtins/cd_builtin.c
@@ -1,11 +1,27 @@
 #include "minishell.h"
 
+void update_oldpwd(char *p,  char **env)
+{
+    int i;
+    char *new_opwd;
+
+    new_opwd = ft_strjoin("OLDPWD=", p);
+    i = 0;
+    while (env[i])
+    {
+        if (ft_strncmp("OLDPWD=", env[i], 8) == 0)
+        {
+            env[i] = 
+        }
+    }
+}
+
 int main(int argc, char **argv) //pasar como argumento PATH de env
 {
     char    t_path[MAX_PATH];
 
     getcwd(t_path, MAX_PATH);
-	if (strncmp(env.path, "-")) // si es cd -, moverse al OLDPWD
+	if (ft_strncmp(env.path, "-")) // si es cd -, moverse al OLDPWD
 		return (cd_oldpwd(t_path, env));
     if (chdir(argv[1]) == 0)
 	{
