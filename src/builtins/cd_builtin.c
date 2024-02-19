3598b7dd894007dd4c48761f3bb2d49a6f71503e
Alejandro Diaz-Ufano Perez <adiazufano13@gmail.com>
2024-02-19 19:26:18 +0100

    checkout

--- src/builtins/cd_builtin.c
+++ src/builtins/cd_builtin.c
@@ -1,28 +1,31 @@
 #include "minishell.h"
+void update_pwd(char *p, t_data *data)
+{
 
-void update_oldpwd(char *p,  char **env)
+}
+void update_oldpwd(char *p,  t_data *data)
 {
     int i;
     char *new_opwd;
 
     new_opwd = ft_strjoin("OLDPWD=", p);
     i = 0;
-    while (env[i])
+    while (&data->envlist[i])
     {
-        if (ft_strncmp("OLDPWD=", env[i], 8) == 0)
+        if (ft_strncmp("OLDPWD=", &data->envlist[i], 8) == 0)
         {
-            env[i] = 
+           &data->envlist. = new_opwd;
         }
     }
 }
 
-int main(int argc, char **argv) //pasar como argumento PATH de env
+int cd_builtin(t_data *data, char *argv) //pasar como argumento PATH de env
 {
     char    t_path[MAX_PATH];
 
     getcwd(t_path, MAX_PATH);
-	if (ft_strncmp(env.path, "-")) // si es cd -, moverse al OLDPWD
-		return (cd_oldpwd(t_path, env));
+	if (ft_strncmp(argv, "-")) // si es cd -, moverse al OLDPWD
+		return (cd_oldpwd(t_path, data));
     if (chdir(argv[1]) == 0)
 	{
 		update_oldpwd(t_path, argv[1]); // actualiza la variable OLDPWD del ENV
