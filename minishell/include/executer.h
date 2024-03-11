d50664c1a6105490e7f400021dc9b7e3ee4e41ef
peterm-m <petermartinezmoreno@gmail.com>
2024-03-11 01:49:07 +0100

    WIP: executer

--- minishell/include/executer.h
+++ minishell/include/executer.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executer.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pedromar <pedromar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 15:56:58 by pedromar          #+#    #+#             */
-/*   Updated: 2024/03/10 20:16:07 by pedromar         ###   ########.fr       */
+/*   Updated: 2024/03/11 01:43:22 by pedro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define EXECUTER_H
 
 # include "minishell.h"
+# include <limits.h>
 
 typedef struct s_file_name
 {
@@ -25,7 +26,7 @@ typedef struct s_path_name
 	char	path_name[PATH_MAX];
 }	t_path_name;
 
-void	executer(void);
+void	executer(t_simple *cmd);
 //void	expander(t_data *data, char *input);
 
 #endif
--- minishell/src/executer/executer.c
+++ minishell/src/executer/executer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executer.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pedromar <pedromar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 15:56:16 by pedromar          #+#    #+#             */
-/*   Updated: 2024/03/10 20:21:56 by pedromar         ###   ########.fr       */
+/*   Updated: 2024/03/11 01:46:23 by pedro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,12 @@
 	else path emty string
 */
 
-void	search_path(t_file_name *name, t_path_name *path)
+// TODO: testear que pasa si NULL en input
+//		 testear PATH no existe
+//		 testear que pasa si no se encuentra dir
+//		 testear que pasa si existe pero sin permisos de ejecucion
+
+void	search_path(char *name, t_path_name *path)
 {
 	int			i;
 	int			find;
@@ -32,7 +37,7 @@ void	search_path(t_file_name *name, t_path_name *path)
 		{
 			ft_strlcpy(path->path_name, directorys[i], PATH_MAX);
 			ft_strlcat(path->path_name, "/", PATH_MAX);
-			ft_strlcat(path->path_name, name->file_name, PATH_MAX);
+			ft_strlcat(path->path_name, name, PATH_MAX);
 			if (access(path->path_name, X_OK) == 0)
 				find = 1;
 		}
@@ -43,7 +48,59 @@ void	search_path(t_file_name *name, t_path_name *path)
 	ft_free(directorys);
 }
 
-void	executer(void)
+char 	**list_to_arr(t_word_list *words)
 {
+	size_t		n_word;
+	char		**arr;
+	t_word_list	*tmp;
+
+	n_word = 0;
+	tmp = words; 
+	while (tmp)
+	{
+		tmp = tmp->next;
+		n_word++;
+	}
+	arr = ft_malloc((n_word +1) * sizeof(char *));
+	arr[n_word] = NULL;
+	n_word = 0;
+	tmp = words;
+	while (tmp)
+	{
+		arr[n_word++] = tmp->word;
+		tmp = tmp->next;
+	}
+	return (arr);
+}
+
+// Si no nombre del comando no tiene barras, intenta localizarlo en las funciones
+// Si no es una funcion. Se busca en la lista de built-in
+// Si no funcion ni built-in busca en PATH. Mirar descripcion 
+// Si la busqueda es satisfactoria o si el nombre tiene barras, se ejecuta en shell por separado
+// 		usando el resto de argumentos como parametros.
+// Si falla 
+// Si no se lanza de forma asincrona, la shell espera a que se complete el comando y recopila su salida.
+	
+
+void	executer(t_simple *cmd)
+{
+	pid_t		pid;
+	t_path_name	filename;
+	char		**argv;
+
+	search_path(cmd->words->word, &filename);
+	argv = list_to_arr(cmd->words);
+	pid = ft_fork();
+	if (pid == 0)
+	{
+		(void) filename;
+		(void) argv;
+		(void) environ;
+	 	exit(0);
+	}
+	else
+	{
+		(void) 0;
+	}
 	return ;
 }
