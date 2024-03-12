e5fedc92c7758552e7371c4dc1b8dcb2047674fa
pedromar <petermartinezmoreno@gmail.com>
2024-03-12 20:25:00 +0100

    clean subsystem

--- minishell/include/subsystem.h
+++ minishell/include/subsystem.h
@@ -6,7 +6,7 @@
 /*   By: pedromar <pedromar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 15:56:58 by pedromar          #+#    #+#             */
-/*   Updated: 2024/03/12 19:42:06 by pedromar         ###   ########.fr       */
+/*   Updated: 2024/03/12 20:24:34 by pedromar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 # define SUBSYSTEM_H
 
 # include "minishell.h"
-
-void	subsystem(void);
 
 typedef struct s_env
 {
@@ -25,15 +23,5 @@ typedef struct s_env
 
 void	initial_signals(void);
 void	sigint_handler(int sig);
-
-//get_env.c
-t_dlst	*get_enviroment(const char **env, t_dlst **head);
-char	**make_char(t_dlst *head);
-
-// env_list_utils.c
-t_env	*new_t_env(void);
-void	free_t_env(t_env *tok);
-void	open_redir(t_redirect *redirections);
-void	make_redir(t_redirect *redirections);
 
 #endif
