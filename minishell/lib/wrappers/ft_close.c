529fbee5f0aa6ed58598a4ed07b46c9c84bd2bc1
pedromar <petermartinezmoreno@gmail.com>
2024-03-20 19:48:55 +0100

    WIP

--- minishell/lib/wrappers/ft_close.c
+++ minishell/lib/wrappers/ft_close.c
@@ -6,7 +6,7 @@
 /*   By: pedromar <pedromar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 16:07:17 by pedromar          #+#    #+#             */
-/*   Updated: 2023/12/02 16:20:25 by pedromar         ###   ########.fr       */
+/*   Updated: 2024/03/20 19:32:26 by pedromar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_close(int fd)
 {
 	int	rc;
-
+	printf("LOG %d\n", fd);
 	rc = close(fd);
 	if (rc < 0)
 		unix_error("Close error");
