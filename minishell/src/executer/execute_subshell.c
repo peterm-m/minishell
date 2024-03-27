efa3745e3b6474a58de2a01dcf710962ba86b546
peterm-m <petermartinezmoreno@gmail.com>
2024-03-27 18:50:59 +0100

    FIX subshell

--- minishell/src/executer/execute_subshell.c
+++ minishell/src/executer/execute_subshell.c
@@ -6,13 +6,13 @@
 /*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 20:53:24 by pedromar          #+#    #+#             */
-/*   Updated: 2024/03/22 17:59:41 by pedro            ###   ########.fr       */
+/*   Updated: 2024/03/27 18:46:39 by pedro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	execute_subshell(t_subshell *cmd)
+void	execute_subshell(t_subshell *cmd, int fd_in, int fd_out)
 {
 	pid_t		pid;
 
@@ -20,7 +20,7 @@ void	execute_subshell(t_subshell *cmd)
 	if (pid == 0)
 	{
 		child_signals();
-		execute_command(cmd->command); // TODO: investigar si subshell se hace esto
+		execute_command(cmd->command, fd_in, fd_out); // TODO: investigar si subshell se hace esto
 	}
 	else
 	{
