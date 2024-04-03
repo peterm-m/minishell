89e069fce1f962f891a88d1b16d738196277c264
Alejandro Diaz-Ufano Perez <adiazufano13@gmail.com>
2024-04-03 19:04:10 +0200

    WIP

--- minishell/src/grammar/semantics/redirections.c
+++ minishell/src/grammar/semantics/redirections.c
@@ -6,39 +6,12 @@
 /*   By: adiaz-uf <adiaz-uf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 01:08:35 by pedro             #+#    #+#             */
-/*   Updated: 2024/04/02 20:50:00 by adiaz-uf         ###   ########.fr       */
+/*   Updated: 2024/04/03 19:03:22 by adiaz-uf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void heredoc(t_dlst **lex, char *delimiter)
-{
-	char *line;
-	char *full_history;
-	int		size;
-	t_token	*token;
-
-	line = NULL;
-	full_history = NULL;
-	size = ft_strlen(delimiter);
-	full_history = ft_strjoin(full_history, delimiter);
-	full_history = ft_strjoin(full_history, "\n");
-	while (ft_strncmp(line, delimiter, size) != 0)
-	{
-		line = readline(BHCYN"heredoc>"END);
-		full_history = ft_strjoin(full_history, line);
-		full_history = ft_strjoin(full_history, "\n");
-		//free(line);
-	}
-	//full_history = ft_strjoin(full_history, delimiter);
-	if (is_quotes(delimiter[0]) && is_quotes(delimiter[size]))
-		printf("quotes\n");
-		//expand(full_history);
-	token = new_token();
-	set_token(full_history, ft_strlen(full_history), tt_dgreat, token);
-	ft_dlstaddb(lex, ft_dlstnew(token));
-}
 #undef LOGS
 #define LOGS 0
 
