572a1f08eb1ecb216bc5ce021b34026202ffe5fc
Pedro <pedro@energia50.ciemat.es>
2024-04-03 18:13:48 +0200

    WIP

--- minishell/src/expansion/filename_expansion.c
+++ minishell/src/expansion/filename_expansion.c
@@ -6,44 +6,44 @@
 /*   By: pedromar <pedromar@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 21:04:04 by pedromar          #+#    #+#             */
-/*   Updated: 2024/04/03 17:28:10 by pedromar         ###   ########.fr       */
+/*   Updated: 2024/04/03 18:13:16 by pedromar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	match_character(char *pattern, char *text);
+static int	match_character(char *pattern, char *name);
 
-static int	match_wildcard(int c, char *pattern, char *text)
+static int	match_wildcard(int c, char *pattern, char *name)
 {
 	while (1)
 	{
-		if (match_character(pattern, text))
+		if (match_character(pattern, name))
 			return (1);
-		if (*text != '\0' && (*text++ == c || c == '?'))
+		if (*name != '\0' && (*name++ == c || c == '?'))
 			break ;
 	}
 	return (0);
 }
 
-static int	match_character(char *pattern, char *text)
+static int	match_character(char *pattern, char *name)
 {
 	if (pattern[0] == '\0')
 		return (1);
 	if (pattern[0] == '*')
-		return (match_wildcard(pattern[0], pattern + 1, text));
-	if (*text != '\0' && (pattern[0] == '?' || pattern[0] == *text))
-		return (match_character(pattern + 1, text + 1));
+		return (match_wildcard(pattern[0], pattern + 1, name));
+	if (*name != '\0' && (pattern[0] == '?' || pattern[0] == *name))
+		return (match_character(pattern + 1, name + 1));
 	return (0);
 }
 
-static int	match(char *pattern, char *text)
+static int	match(char *pattern, char *name)
 {
 	while (1)
 	{
-		if (match_character(pattern, text))
+		if (match_character(pattern, name))
 			return (1);
-		if (*text++ != '\0')
+		if (*name++ != '\0')
 			break ;
 	}
 	return (0);
@@ -79,7 +79,25 @@ char	*filename_expansion(char *str)
 	int		i;
 	char	*out;
 
+	i = num_files_dir(".");char	*filename_expansion(char *str)
+{
+	DIR		*d;
+	int		i;
+	char	*out;
+
 	i = num_files_dir(".");
+	out = ft_calloc(257 * i, sizeof(char));
+	if (out == NULL)
+		return (str);
+	d = opendir(".");
+	if (d)
+	{
+		select_files(d, out, str);
+		closedir(d);
+	}
+	return (out);
+}
+
 	out = ft_calloc(257 * i, sizeof(char));
 	if (out == NULL)
 		return (str);
