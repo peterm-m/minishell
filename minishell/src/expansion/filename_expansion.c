bd8a17f014b36eec06782db40e8e4282c2e5d1b2
Pedro <pedro@energia50.ciemat.es>
2024-04-04 17:34:46 +0200

    FIX:arreglado bucle de filename_expansion

--- minishell/src/expansion/filename_expansion.c
+++ minishell/src/expansion/filename_expansion.c
@@ -6,7 +6,7 @@
 /*   By: pedromar <pedromar@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 21:04:04 by pedromar          #+#    #+#             */
-/*   Updated: 2024/04/03 18:13:16 by pedromar         ###   ########.fr       */
+/*   Updated: 2024/04/04 17:33:42 by pedromar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static int	match_wildcard(int c, char *pattern, char *name)
 	{
 		if (match_character(pattern, name))
 			return (1);
-		if (*name != '\0' && (*name++ == c || c == '?'))
+		if (*name == '\0' || (*name++ == c || c == '?'))
 			break ;
 	}
 	return (0);
@@ -31,7 +31,7 @@ static int	match_character(char *pattern, char *name)
 	if (pattern[0] == '\0')
 		return (1);
 	if (pattern[0] == '*')
-		return (match_wildcard(pattern[0], pattern + 1, name));
+		return (match_wildcard(pattern[1], pattern + 1, name));
 	if (*name != '\0' && (pattern[0] == '?' || pattern[0] == *name))
 		return (match_character(pattern + 1, name + 1));
 	return (0);
@@ -39,18 +39,18 @@ static int	match_character(char *pattern, char *name)
 
 static int	match(char *pattern, char *name)
 {
-	while (1)
+	while (*name)
 	{
 		if (match_character(pattern, name))
 			return (1);
-		if (*name++ != '\0')
+		if (*name++ == '\0')
 			break ;
 	}
 	return (0);
 }
 
 
-// TODO user funcion get_file_names que la cadena se forma ordenada 
+// TODO usar funcion get_file_names que la cadena se forma ordenada 
 static void	select_files(DIR *d, char *file_name, char *str)
 {
 	char			*pattern;
@@ -66,7 +66,7 @@ static void	select_files(DIR *d, char *file_name, char *str)
 			continue ;
 		if (match(pattern, dir->d_name))
 		{
-			file_name += ft_strlcpy(file_name, dir->d_name, 256);
+			file_name += ft_strlcpy(file_name, dir->d_name, PATH_MAX);
 			*file_name++ = ' ';
 		}
 		pattern = str;
@@ -79,26 +79,8 @@ char	*filename_expansion(char *str)
 	int		i;
 	char	*out;
 
-	i = num_files_dir(".");char	*filename_expansion(char *str)
-{
-	DIR		*d;
-	int		i;
-	char	*out;
-
 	i = num_files_dir(".");
-	out = ft_calloc(257 * i, sizeof(char));
-	if (out == NULL)
-		return (str);
-	d = opendir(".");
-	if (d)
-	{
-		select_files(d, out, str);
-		closedir(d);
-	}
-	return (out);
-}
-
-	out = ft_calloc(257 * i, sizeof(char));
+	out = ft_calloc((PATH_MAX +1) * i, sizeof(char));
 	if (out == NULL)
 		return (str);
 	d = opendir(".");
