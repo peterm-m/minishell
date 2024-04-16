1906cc4c2635e02bcb9c17432edcf05b6d4e4482
pedromar <petermartinezmoreno@gmail.com>
2024-04-16 20:51:05 +0200

    FIX: executer type  simple y  builtin

--- src/executer/ft_execle.c
+++ src/executer/ft_execle.c
@@ -6,7 +6,7 @@
 /*   By: pedromar <pedromar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 19:23:11 by pedromar          #+#    #+#             */
-/*   Updated: 2024/04/14 15:03:06 by pedromar         ###   ########.fr       */
+/*   Updated: 2024/04/16 20:46:27 by pedromar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,22 @@ void	ft_execle(t_word *args)
 	if (args == NULL)
 		exit(EXIT_SUCCESS);
 	argv = list_to_argv(args);
-	if (get_pathname(args->word, &filename) == 0)
+	if (argv[0] == NULL)
+		(void) 0;
+	else if (get_pathname(argv[0], &filename) == 0)
 	{
-		ft_putstr_fd(args->word, 2);
+		ft_putstr_fd(argv[0], 2);
 		ft_putstr_fd(": Command not found\n", 2);
 		exit(STATUS_CMD_NOT_FOUND);
 	}
 	else if (access(filename.path_name, X_OK) != 0)
 	{
-		ft_putstr_fd(args->word, 2);
+		ft_putstr_fd(argv[0], 2);
 		ft_putstr_fd(": Permission denied\n", 2);
 		exit(STATUS_CMD_NOT_EXEC);
 	}
-	ft_execve(filename.path_name, argv, environ);
+	else
+		ft_execve(filename.path_name, argv, environ);
 	clean_argv(argv);
 	exit(EXIT_SUCCESS);
 }
